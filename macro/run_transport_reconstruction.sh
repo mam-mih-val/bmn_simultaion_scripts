a120fdf6688b2ce1ff96211b1c19116a9b7cf9a7
mam-mih-val <mam.mih.val@gmail.com>
2022-02-16 14:11:56 +0100

    - Macro looks okay. I will try to run it and see if everythig is okay

---
+++ macro/run_transport_reconstruction.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+n_events=$1
+generator=$2
+input_file=\"$3\"
+output_file=\"$4\"
+
+
+export SIMPATH=/cvmfs/fairsoft.gsi.de/debian10/fairsoft/apr21p2/
+export FAIRROOTPATH=/cvmfs/fairsoft.gsi.de/debian10/fairroot/v18.6.7_fs_apr21p2/
+
+. /lustre/hades/user/mmamaev/bmnroot/build/config.sh
+
+export G4ABLADATA=$SIMPATH/share/Geant4-10.7.1/data/G4ABLA3.1/
+export G4LEDATA=$SIMPATH/share/Geant4-10.7.1/data/G4EMLOW7.13/
+export G4LEVELGAMMADATA=$SIMPATH/share/Geant4-10.7.1/data/PhotonEvaporation5.7/
+export G4NEUTRONHPDATA=$SIMPATH/share/Geant4-10.7.1/data/G4NDL4.6/
+export G4PIIDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PII1.3/
+export G4RADIOACTIVEDATA=$SIMPATH/share/Geant4-10.7.1/data/RadioactiveDecay5.6/
+export G4REALSURFACEDATA=$SIMPATH/share/Geant4-10.7.1/data/RealSurface2.2/
+
+root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_sim_bmn.C( $input_file, $output_file, 0, $n_events, $generator )"
+
+echo PROCESS FINISHED
