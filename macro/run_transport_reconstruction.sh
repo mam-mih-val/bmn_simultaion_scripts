3b64b4799ab4768e7e56c51641602bc7c141d854
mam-mih-val <mam.mih.val@gmail.com>
2022-02-16 14:39:35 +0100

    reconstruction step added

--- macro/run_transport_reconstruction.sh
+++ macro/run_transport_reconstruction.sh
@@ -2,8 +2,8 @@
 
 n_events=$1
 generator=$2
-input_file=\"$3\"
-output_file=\"$4\"
+input_file=$3
+output_file=$4
 
 export SIMPATH=/cvmfs/fairsoft.gsi.de/debian10/fairsoft/apr21p2/
 export FAIRROOTPATH=/cvmfs/fairsoft.gsi.de/debian10/fairroot/v18.6.7_fs_apr21p2/
@@ -20,6 +20,14 @@ export G4PIIDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PII1.3/
 export G4RADIOACTIVEDATA=$SIMPATH/share/Geant4-10.7.1/data/RadioactiveDecay5.6/
 export G4REALSURFACEDATA=$SIMPATH/share/Geant4-10.7.1/data/RealSurface2.2/
 
-root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_sim_bmn.C( $input_file, $output_file, 0, $n_events, $generator )"
+str_input_file=\"$input_file\"
+str_output_file=\"$output_file\"
+
+root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_sim_bmn.C( $str_input_file, $str_input_file, 0, $n_events, $generator )"
+
+str_input_file=\"$output_file\"
+str_output_file=\"dst_$output_file\"
+
+root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_reco_bmn.C( $str_input_file, $str_input_file, 0, $n_events )"
 
 echo PROCESS FINISHED
