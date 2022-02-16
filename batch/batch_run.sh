987346d5bd12905e56af70fb0404f3abbad57afb
mam-mih-val <mam.mih.val@gmail.com>
2022-02-16 15:25:58 +0100

    Attemping to run in debug partition

---
+++ batch/batch_run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+cd $output_dir
+mkdir -p $job_num
+cd $job_num
+
+job_num=$(($SLURM_ARRAY_TASK_ID))
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
+
+n_events=10
+input_file=$(head -n 1 $filelist)
+output_file=output.root
+
+cp $input_file .
+gzip -d "$(basename $input_file)"
+input_file="${$(basename $input_file)%.*}.dat"
+
+export SIMPATH=/cvmfs/fairsoft.gsi.de/debian10/fairsoft/apr21p2/
+export FAIRROOTPATH=/cvmfs/fairsoft.gsi.de/debian10/fairroot/v18.6.7_fs_apr21p2/
+
+. /lustre/hades/user/mmamaev/bmnroot/build/config.sh
+
+export G4PARTICLEXSDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PARTICLEXS3.1.1/
+export G4ENSDFSTATEDATA=$SIMPATH/share/Geant4-10.7.1/data/G4ENSDFSTATE2.3/
+export G4ABLADATA=$SIMPATH/share/Geant4-10.7.1/data/G4ABLA3.1/
+export G4LEDATA=$SIMPATH/share/Geant4-10.7.1/data/G4EMLOW7.13/
+export G4LEVELGAMMADATA=$SIMPATH/share/Geant4-10.7.1/data/PhotonEvaporation5.7/
+export G4NEUTRONHPDATA=$SIMPATH/share/Geant4-10.7.1/data/G4NDL4.6/
+export G4PIIDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PII1.3/
+export G4RADIOACTIVEDATA=$SIMPATH/share/Geant4-10.7.1/data/RadioactiveDecay5.6/
+export G4REALSURFACEDATA=$SIMPATH/share/Geant4-10.7.1/data/RealSurface2.2/
+
+str_input_file=\"$input_file\"
+str_output_file=\"$output_file\"
+
+root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
+
+str_input_file=\"$output_file\"
+str_output_file=\"dst_$output_file\"
+
+root -q "/lustre/nyx/hades/user/mmamaev/bmn_simultaion_scripts/src/run_reco_bmn.C( $str_input_file, $str_output_file, 0, $n_events )"
+
+echo PROCESS FINISHED
---
+++ batch/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+n_files=$1
+file_list=$2
+output_dir=$3
+generator=$4
+
+partition=debug
+time=0:20:00
+
+lists_dir=${output_dir}/lists/
+log_dir=${output_dir}/log/
+
+mkdir -p $output_dir
+mkdir -p $log_dir
+mkdir -p $lists_dir
+
+split -l $n_files -d -a 3 --additional-suffix=.list "$file_list" $lists_dir
+
+n_runs=$(ls $lists_dir/*.list | wc -l)
+
+job_range=1-$n_runs
+
+echo file list: $file_list
+echo output_dir: $output_dir
+echo log_dir: $log_dir
+echo lists_dir: $lists_dir
+echo n_runs: $n_runs
+echo job_range: $job_range
+
+sbatch --wait \
+      -J Files \
+      -p $partition \
+      -t $time \
+      -a $job_range \
+      -e ${log_dir}/%A_%a.e \
+      -o ${log_dir}/%A_%a.o \
+      --export=output_dir=$output_dir,file_list=$file_list,lists_dir=$lists_dir,generator=$generator \
+      -- /lustre/nyx/hades/user/mmamaev/hades_yield/batch/yield/batch_run.sh
+
+echo JOBS HAVE BEEN COMPLETED!
