4dafa8423e1725b8984ac5625bb08206238c3816
mam-mih-val <mam.mih.val@gmail.com>
2022-04-12 19:33:40 +0300

    Attempt to submit jobs on nica cluster

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -11,27 +11,27 @@ cd $output_dir
 mkdir -p $job_num
 cd $job_num
 
-n_events=1000
+n_events=10
 input_file=$(head -n 1 $filelist)
 output_file=geat_output.root
 
-source /mnt/pool/nica/7/mam2mih/soft/basov/bmn_environment.sh
+source /scratch1/mmamaev/bmn_environment.sh
 
 str_input_file=\"$input_file\"
 str_output_file=\"$output_file\"
 
-root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run8/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
+root -q "/scratch1/mmamaev/bmnroot/macro/run8/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
 
 str_input_file=\"$output_file\"
 str_output_file=\"dst_$output_file\"
 
-root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run8/run_reco_bmn.C( $str_input_file, $str_output_file, 0, $n_events )"
+root -q "/scratch1/mmamaev/bmnroot/macro/run8/run_reco_bmn.C( $str_input_file, $str_output_file, 0, $n_events )"
 
 str_atree_file=\"atree_$output_file\"
 str_geometry_file=\"full_geometry.root\"
 
-root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/analysis/common/macro/run_analysis_tree_maker.C( $str_output_file, $str_input_file, $str_geometry_file, $str_atree_file )"
+root -q "/scratch1/mmamaev/bmnroot/analysis/common/macro/run_analysis_tree_maker.C( $str_output_file, $str_input_file, $str_geometry_file, $str_atree_file )"
 
-root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/analysis/common/macro/run_analysistree_qa.C( $str_atree_file, true )"
+root -q "/scratch1/mmamaev/bmnroot/analysis/common/macro/run_analysistree_qa.C( $str_atree_file, true )"
 
 echo PROCESS FINISHED
--- macro/run_transport_reconstruction.sh
+++ macro/run_transport_reconstruction.sh
@@ -5,20 +5,7 @@ generator=$2
 input_file=$3
 output_file=$4
 
-export SIMPATH=/cvmfs/fairsoft.gsi.de/debian10/fairsoft/apr21p2/
-export FAIRROOTPATH=/cvmfs/fairsoft.gsi.de/debian10/fairroot/v18.6.7_fs_apr21p2/
-
-source /lustre/hades/user/mmamaev/bmnroot/build/config.sh
-
-export G4PARTICLEXSDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PARTICLEXS3.1.1/
-export G4ENSDFSTATEDATA=$SIMPATH/share/Geant4-10.7.1/data/G4ENSDFSTATE2.3/
-export G4ABLADATA=$SIMPATH/share/Geant4-10.7.1/data/G4ABLA3.1/
-export G4LEDATA=$SIMPATH/share/Geant4-10.7.1/data/G4EMLOW7.13/
-export G4LEVELGAMMADATA=$SIMPATH/share/Geant4-10.7.1/data/PhotonEvaporation5.7/
-export G4NEUTRONHPDATA=$SIMPATH/share/Geant4-10.7.1/data/G4NDL4.6/
-export G4PIIDATA=$SIMPATH/share/Geant4-10.7.1/data/G4PII1.3/
-export G4RADIOACTIVEDATA=$SIMPATH/share/Geant4-10.7.1/data/RadioactiveDecay5.6/
-export G4REALSURFACEDATA=$SIMPATH/share/Geant4-10.7.1/data/RealSurface2.2/
+source /mnt/pool/nica/7/mam2mih/soft/basov/bmn_environment.sh
 
 str_input_file=\"$input_file\"
 str_output_file=\"$output_file\"
