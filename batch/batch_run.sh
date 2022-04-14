ee38eb8f4b29d74696326670826a32b6735e25de
mam-mih-val <mam.mih.val@gmail.com>
2022-04-15 02:02:09 +0300

    Reconstruction + conversion steps

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -20,7 +20,7 @@ source /mnt/pool/nica/7/mam2mih/soft/basov/bmn_environment.sh
 str_input_file=\"$input_file\"
 str_output_file=\"$output_file\"
 
-root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run8/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
+#root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run8/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
 
 str_input_file=\"$output_file\"
 str_output_file=\"dst_$output_file\"
