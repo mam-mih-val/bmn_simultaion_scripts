6fec679c986272b9f345d0214b0ccf56b58fdeeb
mam-mih-val <mam.mih.val@gmail.com>
2022-05-28 00:17:21 +0300

    The full simulation chain

--- batch/batch_run.sh
+++ batch/batch_run.sh
@@ -20,17 +20,17 @@ source /mnt/pool/nica/7/mam2mih/soft/basov/bmn_environment.sh
 str_input_file=\"$input_file\"
 str_output_file=\"$output_file\"
 
-#root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
+root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run/run_sim_bmn.C( $str_input_file, $str_output_file, 0, $n_events, $generator )"
 
 str_input_file=\"$output_file\"
 str_output_file=\"dst2_$output_file\"
 
-#root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run/run_reco_bmn.C( $str_input_file, $str_output_file, 0, $n_events )"
+root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/macro/run/run_reco_bmn.C( $str_input_file, $str_output_file, 0, $n_events )"
 
 str_atree_file=\"atree2_$output_file\"
 str_geometry_file=\"full_geometry.root\"
 
-#root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/analysis/common/macro/run_analysis_tree_maker.C( $str_output_file, $str_input_file, $str_geometry_file, $str_atree_file )"
+root -q "/mnt/pool/nica/7/mam2mih/soft/basov/bmnroot-mamaev/analysis/common/macro/run_analysis_tree_maker.C( $str_output_file, $str_input_file, $str_geometry_file, $str_atree_file )"
 
 qa_file=\"tracking_qa.root\"
 
