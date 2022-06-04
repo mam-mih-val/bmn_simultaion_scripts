a08c6c8869b8829360abe88d9580c8696bab1ba2
mam-mih-val <mam.mih.val@gmail.com>
2022-06-04 15:01:30 +0300

    Batch scripts for QnAnalysis added

---
+++ qn_analysis/batch_run.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+job_num=$(($SGE_TASK_ID))
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
+
+cd $output_dir
+mkdir -p $job_num
+cd $job_num
+
+source /cvmfs/nica.jinr.ru/sw/os/login.sh
+module add GCC-Toolchain/
+
+source /scratch1/mmamaev/bmn_environment.sh
+echo
+date
+echo "Running preprocessing ..."
+/scratch1/mmamaev/bmn_preprocessing/build/src/pre_process \
+                                                        -i $filelist \
+                                                        -t rTree \
+                                                        --output-tree-name eTree \
+                                                        -o extra.root
+ls $(pwd)/extra.root > extra.list
+echo
+date
+echo "Running correction 1st step ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i $filelist extra.list \
+                                                                          -t rTree eTree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_corrections.yml \
+                                                                          --yaml-config-name BMN
+mv correction_out.root correction_in.root
+echo
+date
+echo "Running correction 2nd step ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i $filelist extra.list \
+                                                                          -t rTree eTree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_corrections.yml \
+                                                                          --yaml-config-name BMN
+mv correction_out.root correction_in.root
+echo
+date
+echo "Running correction 3d step ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+                                                                          -i $filelist extra.list \
+                                                                          -t rTree eTree \
+                                                                          -o correction_out.root \
+                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_corrections.yml \
+                                                                          --yaml-config-name BMN
+echo
+date
+echo "Running correlation ..."
+/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
+                                                                              --input-file correction_in.root \
+                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_correlations.yml \
+                                                                              --configuration-name _tasks \
+                                                                              -o correlation_out.root
+
+echo PROCESS FINISHED
---
+++ qn_analysis/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+file_list=$1
+output_dir=$2
+n_files=$3
+
+time=14:00:00
+
+lists_dir=${output_dir}/lists/
+log_dir=${output_dir}/log/
+
+mkdir -p $output_dir
+mkdir -p $log_dir
+mkdir -p $lists_dir
+
+split -l $n_files -d -a 5 --additional-suffix=.list "$file_list" $lists_dir
+
+n_runs=$(ls $lists_dir/*.list | wc -l)
+
+job_range=1-$n_runs
+
+echo file list: $file_list
+echo output_dir: $output_dir
+echo log_dir: $log_dir
+echo lists_dir: $lists_dir
+echo n_runs: $n_runs
+echo job_range: $job_range
+
+qsub  -wd $PWD -cwd \
+      -N QnAnalysis \
+      -l h_rt=$time \
+      -l s_rt=$time \
+      -t $job_range \
+      -e ${log_dir}/ \
+      -o ${log_dir}/ \
+      -v output_dir=$output_dir,file_list=$file_list,lists_dir=$lists_dir \
+      /scratch1/mmamaev/bmn_simultaion_scripts/qn_analysis/batch_run.sh
+
+echo JOBS HAVE BEEN SUBMITTED!
