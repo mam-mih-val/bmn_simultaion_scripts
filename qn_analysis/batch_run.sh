0aba2ae999c1918791c8c723d0ae0d282acd1cce
mam-mih-val <mam.mih.val@gmail.com>
2022-06-16 01:23:45 +0300

    Run for pure MC data

--- qn_analysis/batch_run.sh
+++ qn_analysis/batch_run.sh
@@ -18,48 +18,54 @@ module add GCC-Toolchain/
 source /scratch1/mmamaev/bmn_environment.sh
 echo
 date
-echo "Running preprocessing ..."
-/scratch1/mmamaev/bmn_preprocessing/build/src/pre_process \
-                                                        -i $filelist \
-                                                        -t rTree \
-                                                        --output-tree-name eTree \
-                                                        -o extra.root
-ls $(pwd)/extra.root > extra.list
-echo
-date
-echo "Running correction 1st step ..."
+#echo "Running preprocessing ..."
+#/scratch1/mmamaev/bmn_preprocessing/build/src/pre_process \
+#                                                        -i $filelist \
+#                                                        -t rTree \
+#                                                        --output-tree-name eTree \
+#                                                        -o extra.root
+#ls $(pwd)/extra.root > extra.list
+#echo
+#date
+#echo "Running correction 1st step ..."
+#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+#                                                                          -i $filelist extra.list \
+#                                                                          -t rTree eTree \
+#                                                                          -o correction_out.root \
+#                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
+#                                                                          --yaml-config-name BMN
+#mv correction_out.root correction_in.root
+#echo
+#date
+#echo "Running correction 2nd step ..."
+#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+#                                                                          -i $filelist extra.list \
+#                                                                          -t rTree eTree \
+#                                                                          -o correction_out.root \
+#                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
+#                                                                          --yaml-config-name BMN
+#mv correction_out.root correction_in.root
+#echo
+#date
+#echo "Running correction 3d step ..."
+#/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
+#                                                                          -i $filelist extra.list \
+#                                                                          -t rTree eTree \
+#                                                                          -o correction_out.root \
+#                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
+#                                                                          --yaml-config-name BMN
 /scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-                                                                          -i $filelist extra.list \
-                                                                          -t rTree eTree \
+                                                                          -i $filelist \
+                                                                          -t atree \
                                                                           -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
-                                                                          --yaml-config-name BMN
-mv correction_out.root correction_in.root
-echo
-date
-echo "Running correction 2nd step ..."
-/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-                                                                          -i $filelist extra.list \
-                                                                          -t rTree eTree \
-                                                                          -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
-                                                                          --yaml-config-name BMN
-mv correction_out.root correction_in.root
-echo
-date
-echo "Running correction 3d step ..."
-/scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrect/QnAnalysisCorrect \
-                                                                          -i $filelist extra.list \
-                                                                          -t rTree eTree \
-                                                                          -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
+                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_corrections.yml \
                                                                           --yaml-config-name BMN
 echo
 date
 echo "Running correlation ..."
 /scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
                                                                               --input-file correction_out.root \
-                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_correlations.yml \
+                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/mc_correlations.yml \
                                                                               --configuration-name _tasks \
                                                                               -o correlation_out.root
 
