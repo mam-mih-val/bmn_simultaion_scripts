7de4bd3afb915e6c228023f9ad938c46689f19d7
mam-mih-val <mam.mih.val@gmail.com>
2022-07-13 15:29:35 +0300

    MC setup

--- qn_analysis/batch_run.sh
+++ qn_analysis/batch_run.sh
@@ -32,7 +32,7 @@ echo "Running correction 1st step ..."
                                                                           -i $filelist extra.list \
                                                                           -t rTree eTree \
                                                                           -o correction_out.root \
-                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_corrections.yml \
+                                                                          --yaml-config-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_corrections.yml \
                                                                           --yaml-config-name BMN
 #mv correction_out.root correction_in.root
 #echo
@@ -60,7 +60,7 @@ date
 echo "Running correlation ..."
 /scratch1/mmamaev/QnAnalysis/build/src/QnAnalysisCorrelate/QnAnalysisCorrelate \
                                                                               --input-file correction_out.root \
-                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_correlations.yml \
+                                                                              --configuration-file /scratch1/mmamaev/QnAnalysis/setups/bmatn/xecs_mc_correlations.yml \
                                                                               --configuration-name _tasks \
                                                                               -o correlation_out.root
 
