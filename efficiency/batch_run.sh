afcf1d581fe8c6d12aa36e7b3b99a6c0e8eaf02a
mam-mih-val <mam.mih.val@gmail.com>
2022-07-13 20:00:11 +0300

    QA is added

---
+++ efficiency/batch_run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#$ -l h=!(ncx182.jinr.ru|ncx211.jinr.ru)
+
+format='+%Y/%m/%d-%H:%M:%S'
+
+date $format
+
+job_num=$(($SGE_TASK_ID))
+filelist=$lists_dir/$(ls $lists_dir | sed "${job_num}q;d")
+
+cd $output_dir
+mkdir -p $job_num
+cd $job_num
+
+source /cvmfs/nica.jinr.ru/sw/os/login.sh
+module add GCC-Toolchain/
+
+source /scratch1/mmamaev/bmn_environment.sh
+echo
+date
+echo "Running preprocessing ..."
+
+input_list=\"$filelist\"
+output_file=\"$filelist\"
+root -q "/scratch1/mmamaev/bmnroot/analysis/common/macro/run_tracking_qa.C( $input_list, $output_file )"
+
+echo PROCESS FINISHED
